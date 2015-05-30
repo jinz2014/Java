8039c71b42284c38b0f293fb77ce98939dd668e4
jinz <jinz@email.sc.edu>
2015-05-30 10:10:56 -0400

    Add Enum methods

--- Basics/WeekdayList.java
+++ Basics/WeekdayList.java
@@ -1,5 +1,4 @@
 /**
-  * enum is a class!
   * 
   * values return an array of enumeration constants.
   * When printing the element d, its toString method
@@ -7,7 +6,7 @@
   * 
   */
 
-enum Day {
+enum Day { // enum is a class
   MON,TUE,WED,THU,FRI,SAT,SUN;
 
   @Override
@@ -19,9 +18,40 @@ enum Day {
 
 public class WeekdayList {
   public static void main(String[] args) {
+    //--------------------------------------------
+    // all the constants of an enum type can be 
+    // obtained by calling the values() method
+    //--------------------------------------------
     for (Day d : Day.values()) {
       System.out.print(d + " ");
     }
     System.out.println();
+
+    //--------------------------------------------
+    // convert a String representation of an enum
+    // into an enum type
+    //
+    // public static T valueOf(String)
+    //--------------------------------------------
+
+    Day monday = Day.valueOf("MON");  // or args[0]
+    System.out.println(monday);
+
+    //--------------------------------------------
+    // compare
+    //--------------------------------------------
+    Day mon    = Day.valueOf("MON");
+    Day tuesday = Day.valueOf("TUE");
+    if (monday == tuesday) // false
+      System.out.println("monday and tuesday are the same day");
+
+    if (mon == monday)  // true
+      System.out.println("mon and monday are the same day");
+
+    //--------------------------------------------
+    // convert a enum type to its (default/customized)
+    // String representation
+    //--------------------------------------------
+      System.out.println(monday.toString());
   }
 }
