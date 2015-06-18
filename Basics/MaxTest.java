f1062844fc986a5869f1f3146b0c9fffb30f13c9
jinz <jinz@email.sc.edu>
2015-06-18 10:56:01 -0400

    Return index of the max value in an array

--- Basics/MaxTest.java
+++ Basics/MaxTest.java
@@ -1,13 +1,15 @@
-// Generic class
+/**
+ * Generic class for finding a max in an array
+ *
+ */
+import java.util.Arrays;
+
 public class MaxTest<T extends Comparable> {
-  public int max(T[] a) {
+  public int maxIndexOf(T[] a) {
     if (a.length < 1) 
       return -1;
 
     int pos = 0;
-
-    if (a.length == 1) 
-      return pos;
 
     for (int i = 1; i < a.length; i++) {
       if (a[i].compareTo(a[pos]) > 0)
@@ -20,10 +22,12 @@ public class MaxTest<T extends Comparable> {
     //Float[] a = {1,2, 4, 7, 3, 1}; // error
     Float[] a = {1.0f, 2.0f, 4.0f, 7.0f, 3.0f, 1.0f}; // error
     MaxTest<Float> r1 = new MaxTest<>();
-    System.out.println(r1.max(a));
+    System.out.println(Arrays.toString(a));
+    System.out.println("index of max number: " + r1.maxIndexOf(a));
 
     Integer[] b = {1,2, 3, 4, 7, 1};
     MaxTest<Integer> r2 = new MaxTest<>();
-    System.out.println(r2.max(b));
+    System.out.println(Arrays.toString(b));
+    System.out.println("index of max number: " + r2.maxIndexOf(b));
   }
 }
